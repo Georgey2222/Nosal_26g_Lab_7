dab7242cc9aa4a1ee3b2c1e1c6aa3555ef405c31
heorhii_nosal <nocalko@gmail.com>
2025-06-02 22:36:47 +0300

    end commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import Header from './components/Header/Header.js';
 import books from './books.json'
 import './styles/global.css';
 import Card from './components/Card/Card.js';
 function App() {
+  useEffect(() => {
+    function updatesize() {
+      const width = document.documentElement.clientWidth;
+      const height = document.documentElement.clientHeight;
+      const gorizontal_size = width / 167;
+      const vertical_size = 100;
+      const size = Math.min(gorizontal_size, vertical_size);
+      document.documentElement.style.setProperty('--size', `${size}px`);
+    }
+
+    updatesize();
+    window.addEventListener('resize', updatesize);
+
+    return () => {
+      window.removeEventListener('resize', updatesize);
+    };
+  }, []);
   const [selectedCategory, setSelectedCategory] = useState('All');
     const filteredBooks =
     selectedCategory === 'All'
