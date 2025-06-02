ee07b01963503c9d6364538e0b38ff1771e5d983
Georgey2222 <nocalko@gmail.com>
2025-06-02 23:24:37 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,13 +8,13 @@ function Header()
         </div>
         <nav>
             <a href="./goods.html">
-                <img src="/magnifier_glass.png"/>
+                <img src="./magnifier_glass.png"/>
             </a>
             <a>
-                <img src="/profile.png"/>
+                <img src="./profile.png"/>
             </a>
             <a href="./cart.html">
-                <img src="/cart.png"/>
+                <img src="./cart.png"/>
             </a>
         </nav>
     </header>   
